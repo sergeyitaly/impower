1f64d82290746f34ae6bcafadb643022a5321dc8
sergeyitaly <sergeyitaly@yahoo.com>
2025-02-15 00:44:20 +0200

    host update

--- main.py
+++ main.py
@@ -641,4 +641,4 @@ async def get_index(request: Request):
 # Start FastAPI app
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
